0716f12b546b98c0f5b980d9d49249db3e38384b
sh-mac <dltkdgns3435@naver.com>
2021-12-13 10:56:47 +0900

    Initial commit

---
+++ src/main/java/com/knk/knkinfomation/KnkInfomationApplication.java
@@ -0,0 +1,13 @@
+package com.knk.knkinfomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnkInfomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnkInfomationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/knk/knkinfomation/KnkInfomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knk.knkinfomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnkInfomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
