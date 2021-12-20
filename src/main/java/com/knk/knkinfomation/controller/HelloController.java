ee2a1d3b51071558ae52f033f4199073165417f3
HyunjuIm <dear.jjwim@gmail.com>
2021-12-20 10:46:43 +0900

    knk 홈페이지

--- src/main/java/com/knk/knkinfomation/controller/HelloController.java
+++ src/main/java/com/knk/knkinfomation/controller/HelloController.java
@@ -3,16 +3,19 @@ package com.knk.knkinfomation.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
 
-//    @GetMapping("index")
-    @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
+    @GetMapping("index")
     public String getHello(Model model){
         model.addAttribute("data", "이상훈");
+        return "index";
+    }
+
+    @GetMapping("/test1")
+    public String getHelloTest1(Model model){
+        model.addAttribute("data", "테스트");
         return "index";
     }
 
